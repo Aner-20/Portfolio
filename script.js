6d562ded8f04d1150d12d001adf068f87a344751
Aner-20 <foriglio45.andrea@gmail.com>
2023-03-12 19:11:45 +0100

    Initial Setup

---
+++ script.js
@@ -0,0 +1,9 @@
+const firstPhoto = document.querySelector('#photo1');
+const secondPhoto = document.querySelector('#photo2');
+const thirdPhoto = document.querySelector('#photo3');
+const fourthPhoto = document.querySelector('#photo4');
+
+firstPhoto.addEventListener('click', () => window.open("http://127.0.0.1:5500/Projects/A%20better%20number%20guessing%20game.html"));
+secondPhoto.addEventListener('click', () => window.open("http://127.0.0.1:5500/Projects/Accordion%20menu.html"));
+thirdPhoto.addEventListener('click', () => window.open("http://127.0.0.1:5500/Projects/Countdown.html"));
+fourthPhoto.addEventListener('click', () => window.open("http://127.0.0.1:5500/Projects/Password%20generator.html"));
